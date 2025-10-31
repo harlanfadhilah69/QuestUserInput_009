d142e4466097c316e0c84112b6667aee6260c3b8
harlanfadhilah69 <harlancoegg@gmail.com>
2025-10-31 16:18:12 +0700

    membuat button untuk memasukkan nama jenis kelamin dan alamat

--- app/src/main/java/com/example/praktikum_5/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum_5/TeksEdit.kt
@@ -91,5 +91,18 @@ fun FormDataDiri(modifier: Modifier
             thickness = dimensionResource(id = R.dimen.divider_tipis),
             color = Color.DarkGray
         )
+        Button(
+            modifier = Modifier.fillMaxWidth(fraction = 1f),
+
+            enabled = textAlamat.isNotEmpty(),
+            onClick = {
+                nama = textNama
+                jenis = textJK
+                alamat = textAlamat
+            }
+        ) {
+            Text(text = stringResource(id = R.string.submit))
+        }
+
     }
 }
