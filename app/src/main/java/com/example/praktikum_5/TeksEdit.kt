74cd8d9401735f8714b2f35100dcde3c2a1c3571
harlanfadhilah69 <harlancoegg@gmail.com>
2025-10-31 16:21:09 +0700

    menambah ElevatedCard kustom dengan elevasi dan dimensi tetap

--- app/src/main/java/com/example/praktikum_5/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum_5/TeksEdit.kt
@@ -109,6 +109,12 @@ fun FormDataDiri(modifier: Modifier
             color = Color.DarkGray
         )
 
-
+        ElevatedCard (
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.Black),
+            modifier = Modifier
+                .height(height = 100.dp)
+                .width(width = 300.dp)
+        ){}
     }
 }
