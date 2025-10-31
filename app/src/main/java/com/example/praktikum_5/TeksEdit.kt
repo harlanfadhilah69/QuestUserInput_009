df24d67160fb4658b829f643b48b3b3aa9d8961a
harlanfadhilah69 <harlancoegg@gmail.com>
2025-10-31 16:13:48 +0700

    membuat outlinedTextField untuk alamat

--- app/src/main/java/com/example/praktikum_5/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum_5/TeksEdit.kt
@@ -72,5 +72,15 @@ fun FormDataDiri(modifier: Modifier
                 }
             }
         }
+        OutlinedTextField(
+            value = textAlamat,
+            singleLine = true,
+            modifier = Modifier.width(width = 250.dp),
+            label = { Text(text = "Alamat Lengkap") },
+            onValueChange = {
+                textAlamat = it
+            }
+        )
+
     }
 }
